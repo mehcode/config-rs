3af8fb895f002c8e1a3728d64bfe901b3cab3dfc
Ryan Leckey <leckey.ryan@gmail.com>
2017-01-26 19:05:04 -0800

    :memo:

--- src/file/nil.rs
+++ src/file/nil.rs
@@ -1,7 +1,7 @@
 use source::Source;
 use value::Value;
 
-// Nil source that does nothing for optional files
+// Nil source that does nothing to easily allow for optional files
 pub struct Nil {}
 
 impl Source for Nil {
