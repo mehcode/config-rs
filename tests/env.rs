2fe18bbe8716402f6c56101ccbd55f5f1879d884
Christian Fochler <mail@christianfochler.de>
2018-02-13 10:04:55 +0100

    add test for prefix with variant forms of spelling

--- tests/env.rs
+++ tests/env.rs
@@ -26,6 +26,31 @@ fn test_prefix_is_removed_from_key() {
 }
 
 #[test]
+fn test_prefix_with_variant_forms_of_spelling() {
+    env::set_var("a_A_C", "abc");
+
+    let environment = Environment::with_prefix("a");
+
+    assert!(environment.collect().unwrap().contains_key("a_c"));
+
+    env::remove_var("a_A_C");
+    env::set_var("aB_A_C", "abc");
+
+    let environment = Environment::with_prefix("aB");
+
+    assert!(environment.collect().unwrap().contains_key("a_c"));
+
+    env::remove_var("aB_A_C");
+    env::set_var("Ab_A_C", "abc");
+
+    let environment = Environment::with_prefix("ab");
+
+    assert!(environment.collect().unwrap().contains_key("a_c"));
+
+    env::remove_var("Ab_A_C");
+}
+
+#[test]
 fn test_separator_behavior() {
     env::set_var("C_B_A", "abc");
 
