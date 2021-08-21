mehcode/config-rs

commit be82af2a474b9c6ac85ec1e001af1704521820e6

Rename MapImpl to Map

examples/glob/src/main.rs
@@ -1,5 +1,5 @@
 use std::path::Path;
-use std::collections::MapImpl;
+use std::collections::Map;
 use config::*;
 use glob::glob;
 
@@ -14,9 +14,9 @@ fn main() {
         .merge(File::from(Path::new("conf/05-some.yml"))).unwrap()
         .merge(File::from(Path::new("conf/99-extra.json"))).unwrap();
 
-    // Print out our settings (as a MapImpl)
+    // Print out our settings (as a Map)
     println!("\n{:?} \n\n-----------",
-             settings.try_into::<MapImpl<String, String>>().unwrap());
+             settings.try_into::<Map<String, String>>().unwrap());
 
     // Option 2
     // --------
@@ -28,9 +28,9 @@ fn main() {
                     File::from(Path::new("conf/99-extra.json"))])
         .unwrap();
 
-    // Print out our settings (as a MapImpl)
+    // Print out our settings (as a Map)
     println!("\n{:?} \n\n-----------",
-             settings.try_into::<MapImpl<String, String>>().unwrap());
+             settings.try_into::<Map<String, String>>().unwrap());
 
     // Option 3
     // --------
@@ -43,7 +43,7 @@ fn main() {
                    .collect::<Vec<_>>())
         .unwrap();
 
-    // Print out our settings (as a MapImpl)
+    // Print out our settings (as a Map)
     println!("\n{:?} \n\n-----------",
-             settings.try_into::<MapImpl<String, String>>().unwrap());
+             settings.try_into::<Map<String, String>>().unwrap());
 }

examples/async_source/main.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use config::{builder::AsyncState, AsyncSource, ConfigBuilder, ConfigError, FileFormat, MapImpl};
+use config::{builder::AsyncState, AsyncSource, ConfigBuilder, ConfigError, FileFormat, Map};
 
 use async_trait::async_trait;
 use futures::{select, FutureExt};
@@ -56,7 +56,7 @@ struct HttpSource {
 
 #[async_trait]
 impl AsyncSource for HttpSource {
-    async fn collect(&self) -> Result<MapImpl<String, config::Value>, ConfigError> {
+    async fn collect(&self) -> Result<Map<String, config::Value>, ConfigError> {
         reqwest::get(&self.uri)
             .await
             .map_err(|e| ConfigError::Foreign(Box::new(e)))? // error conversion is possible from custom AsyncSource impls

examples/simple/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::MapImpl;
+use std::collections::Map;
 
 fn main() {
     let mut settings = config::Config::default();
@@ -9,7 +9,7 @@ fn main() {
         // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
         .merge(config::Environment::with_prefix("APP")).unwrap();
 
-    // Print out our settings (as a MapImpl)
+    // Print out our settings (as a Map)
     println!("{:?}",
-             settings.try_into::<MapImpl<String, String>>().unwrap());
+             settings.try_into::<Map<String, String>>().unwrap());
 }

examples/watch/src/main.rs
@@ -1,5 +1,5 @@
 use config::*;
-use std::collections::MapImpl;
+use std::collections::Map;
 use std::sync::RwLock;
 use notify::{RecommendedWatcher, DebouncedEvent, Watcher, RecursiveMode};
 use std::sync::mpsc::channel;
@@ -20,7 +20,7 @@ fn show() {
                  .read()
                  .unwrap()
                  .clone()
-                 .try_into::<MapImpl<String, String>>()
+                 .try_into::<Map<String, String>>()
                  .unwrap());
 }
 

src/builder.rs
@@ -2,7 +2,7 @@ use std::iter::IntoIterator;
 use std::str::FromStr;
 
 use crate::error::Result;
-use crate::map::MapImpl;
+use crate::map::Map;
 use crate::source::AsyncSource;
 use crate::{config::Config, path::Expression, source::Source, value::Value};
 
@@ -88,8 +88,8 @@ use crate::{config::Config, path::Expression, source::Source, value::Value};
 /// ```
 #[derive(Debug, Clone, Default)]
 pub struct ConfigBuilder<St: BuilderState> {
-    defaults: MapImpl<Expression, Value>,
-    overrides: MapImpl<Expression, Value>,
+    defaults: Map<Expression, Value>,
+    overrides: Map<Expression, Value>,
     state: St,
 }
 
@@ -121,8 +121,8 @@ pub struct DefaultState {
 /// Refer to [`ConfigBuilder`] for similar API sample usage or to the examples folder of the crate, where such a source is implemented.
 #[derive(Debug, Clone, Default)]
 pub struct AsyncConfigBuilder {
-    defaults: MapImpl<Expression, Value>,
-    overrides: MapImpl<Expression, Value>,
+    defaults: Map<Expression, Value>,
+    overrides: Map<Expression, Value>,
     sources: Vec<SourceType>,
 }
 
@@ -245,11 +245,11 @@ impl ConfigBuilder<DefaultState> {
     }
 
     fn build_internal(
-        defaults: MapImpl<Expression, Value>,
-        overrides: MapImpl<Expression, Value>,
+        defaults: Map<Expression, Value>,
+        overrides: Map<Expression, Value>,
         sources: &[Box<dyn Source + Send + Sync>],
     ) -> Result<Config> {
-        let mut cache: Value = MapImpl::<String, Value>::new().into();
+        let mut cache: Value = Map::<String, Value>::new().into();
 
         // Add defaults
         for (key, val) in defaults.into_iter() {
@@ -323,11 +323,11 @@ impl ConfigBuilder<AsyncState> {
     }
 
     async fn build_internal(
-        defaults: MapImpl<Expression, Value>,
-        overrides: MapImpl<Expression, Value>,
+        defaults: Map<Expression, Value>,
+        overrides: Map<Expression, Value>,
         sources: &[SourceType],
     ) -> Result<Config> {
-        let mut cache: Value = MapImpl::<String, Value>::new().into();
+        let mut cache: Value = Map::<String, Value>::new().into();
 
         // Add defaults
         for (key, val) in defaults.into_iter() {

src/config.rs
@@ -5,7 +5,7 @@ use serde::de::Deserialize;
 use serde::ser::Serialize;
 
 use crate::error::*;
-use crate::map::MapImpl;
+use crate::map::Map;
 use crate::path;
 use crate::ser::ConfigSerializer;
 use crate::source::Source;
@@ -16,8 +16,8 @@ use crate::value::{Table, Value};
 /// them according to the source's priority.
 #[derive(Clone, Debug)]
 pub struct Config {
-    defaults: MapImpl<path::Expression, Value>,
-    overrides: MapImpl<path::Expression, Value>,
+    defaults: Map<path::Expression, Value>,
+    overrides: Map<path::Expression, Value>,
     sources: Vec<Box<dyn Source + Send + Sync>>,
 
     /// Root of the cached configuration.
@@ -83,7 +83,7 @@ impl Config {
     #[deprecated(since = "0.12.0", note = "please use 'ConfigBuilder' instead")]
     pub fn refresh(&mut self) -> Result<&mut Config> {
         self.cache = {
-            let mut cache: Value = MapImpl::<String, Value>::new().into();
+            let mut cache: Value = Map::<String, Value>::new().into();
 
             // Add defaults
             for (key, val) in self.defaults.iter() {
@@ -181,7 +181,7 @@ impl Config {
         self.get(key).and_then(Value::into_bool)
     }
 
-    pub fn get_table(&self, key: &str) -> Result<MapImpl<String, Value>> {
+    pub fn get_table(&self, key: &str) -> Result<Map<String, Value>> {
         self.get(key).and_then(Value::into_table)
     }
 
@@ -212,7 +212,7 @@ impl Source for Config {
         Box::new((*self).clone())
     }
 
-    fn collect(&self) -> Result<MapImpl<String, Value>> {
+    fn collect(&self) -> Result<Map<String, Value>> {
         self.cache.clone().into_table()
     }
 }

src/de.rs
@@ -5,7 +5,7 @@ use serde::de;
 
 use crate::config::Config;
 use crate::error::*;
-use crate::map::MapImpl;
+use crate::map::Map;
 use crate::value::{Table, Value, ValueKind};
 
 impl<'de> de::Deserializer<'de> for Value {
@@ -200,7 +200,7 @@ struct MapAccess {
 }
 
 impl MapAccess {
-    fn new(table: MapImpl<String, Value>) -> Self {
+    fn new(table: Map<String, Value>) -> Self {
         MapAccess {
             elements: table.into_iter().collect(),
         }

src/env.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use crate::error::*;
-use crate::map::MapImpl;
+use crate::map::Map;
 use crate::source::Source;
 use crate::value::{Value, ValueKind};
 
@@ -79,8 +79,8 @@ impl Source for Environment {
         Box::new((*self).clone())
     }
 
-    fn collect(&self) -> Result<MapImpl<String, Value>> {
-        let mut m = MapImpl::new();
+    fn collect(&self) -> Result<Map<String, Value>> {
+        let mut m = Map::new();
         let uri: String = "the environment".into();
 
         let separator = self.separator.as_deref().unwrap_or("");

commit 63fc25b251e9f01baaf3546830393e4441888a58

Fix: Import HashMap instead of Map
I didn't catch that when merging PR #217.
CI should have catched it, but we actually never ran CI for examples.

Signed-off-by: Matthias Beyer <mail@beyermatthias.de>
Fixes: be82af2 ("Rename MapImpl to Map")
Fixes: 0d3a5c3 ("Merge pull request #217 from dlo9/master")

examples/glob/src/main.rs
@@ -1,5 +1,5 @@
 use std::path::Path;
-use std::collections::Map;
+use std::collections::HashMap;
 use config::*;
 use glob::glob;
 
@@ -14,9 +14,9 @@ fn main() {
         .merge(File::from(Path::new("conf/05-some.yml"))).unwrap()
         .merge(File::from(Path::new("conf/99-extra.json"))).unwrap();
 
-    // Print out our settings (as a Map)
+    // Print out our settings (as a HashMap)
     println!("\n{:?} \n\n-----------",
-             settings.try_into::<Map<String, String>>().unwrap());
+             settings.try_into::<HashMap<String, String>>().unwrap());
 
     // Option 2
     // --------
@@ -28,9 +28,9 @@ fn main() {
                     File::from(Path::new("conf/99-extra.json"))])
         .unwrap();
 
-    // Print out our settings (as a Map)
+    // Print out our settings (as a HashMap)
     println!("\n{:?} \n\n-----------",
-             settings.try_into::<Map<String, String>>().unwrap());
+             settings.try_into::<HashMap<String, String>>().unwrap());
 
     // Option 3
     // --------
@@ -43,7 +43,7 @@ fn main() {
                    .collect::<Vec<_>>())
         .unwrap();
 
-    // Print out our settings (as a Map)
+    // Print out our settings (as a HashMap)
     println!("\n{:?} \n\n-----------",
-             settings.try_into::<Map<String, String>>().unwrap());
+             settings.try_into::<HashMap<String, String>>().unwrap());
 }