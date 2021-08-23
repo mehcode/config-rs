f5f529512e4d896995689fa85b91fdf8a5eb1730
Matthias Beyer <mail@beyermatthias.de>
2021-08-23 14:36:27 +0200

    Add tests for wrapping integers
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ tests/integer_range.rs
@@ -0,0 +1,37 @@
+extern crate config;
+
+use config::*;
+
+#[test]
+fn wrapping_u16() {
+    let c = Config::builder()
+        .add_source(config::File::from_str(
+            r#"
+            [settings]
+            port = 66000
+            "#,
+            config::FileFormat::Toml,
+        ))
+        .build()
+        .unwrap();
+
+    let port: u16 = c.get("settings.port").unwrap();
+    assert_eq!(port, 464);
+}
+
+#[test]
+fn nonwrapping_u32() {
+    let c = Config::builder()
+        .add_source(config::File::from_str(
+            r#"
+            [settings]
+            port = 66000
+            "#,
+            config::FileFormat::Toml,
+        ))
+        .build()
+        .unwrap();
+
+    let port: u32 = c.get("settings.port").unwrap();
+    assert_eq!(port, 66000);
+}
