51077f338678d566aabc76543456c8bf6c04a03b
Daniel Eades <danieleades@hotmail.com>
2021-12-27 11:34:46 +0000

    use infallible integer conversions where available

--- src/ser.rs
+++ src/ser.rs
@@ -98,15 +98,15 @@ impl<'a> ser::Serializer for &'a mut ConfigSerializer {
     }
 
     fn serialize_i8(self, v: i8) -> Result<Self::Ok> {
-        self.serialize_i64(v as i64)
+        self.serialize_i64(v.into())
     }
 
     fn serialize_i16(self, v: i16) -> Result<Self::Ok> {
-        self.serialize_i64(v as i64)
+        self.serialize_i64(v.into())
     }
 
     fn serialize_i32(self, v: i32) -> Result<Self::Ok> {
-        self.serialize_i64(v as i64)
+        self.serialize_i64(v.into())
     }
 
     fn serialize_i64(self, v: i64) -> Result<Self::Ok> {
@@ -114,15 +114,15 @@ impl<'a> ser::Serializer for &'a mut ConfigSerializer {
     }
 
     fn serialize_u8(self, v: u8) -> Result<Self::Ok> {
-        self.serialize_u64(v as u64)
+        self.serialize_u64(v.into())
     }
 
     fn serialize_u16(self, v: u16) -> Result<Self::Ok> {
-        self.serialize_u64(v as u64)
+        self.serialize_u64(v.into())
     }
 
     fn serialize_u32(self, v: u32) -> Result<Self::Ok> {
-        self.serialize_u64(v as u64)
+        self.serialize_u64(v.into())
     }
 
     fn serialize_u64(self, v: u64) -> Result<Self::Ok> {
@@ -138,7 +138,7 @@ impl<'a> ser::Serializer for &'a mut ConfigSerializer {
     }
 
     fn serialize_f32(self, v: f32) -> Result<Self::Ok> {
-        self.serialize_f64(v as f64)
+        self.serialize_f64(v.into())
     }
 
     fn serialize_f64(self, v: f64) -> Result<Self::Ok> {
