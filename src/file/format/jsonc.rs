74e5668101417ff25208dd24d450cc7598b6b6c4
up9cloud <8325632+up9cloud@users.noreply.github.com>
2023-09-19 17:47:38 +0000

    Support jsonc format
    Signed-off-by: T.C. <8325632+up9cloud@users.noreply.github.com>

---
+++ src/file/format/jsonc.rs
@@ -0,0 +1,61 @@
+use std::error::Error;
+
+use crate::error::{ConfigError, Unexpected};
+use crate::map::Map;
+use crate::value::{Value, ValueKind};
+
+use jsonc_parser::JsonValue;
+
+pub fn parse(
+    uri: Option<&String>,
+    text: &str,
+) -> Result<Map<String, Value>, Box<dyn Error + Send + Sync>> {
+    match jsonc_parser::parse_to_value(text, &Default::default())? {
+        Some(r) => match r {
+            JsonValue::String(ref value) => Err(Unexpected::Str(value.to_string())),
+            JsonValue::Number(value) => Err(Unexpected::Float(value.parse::<f64>().unwrap())),
+            JsonValue::Boolean(value) => Err(Unexpected::Bool(value)),
+            JsonValue::Object(o) => match from_jsonc_value(uri, JsonValue::Object(o)).kind {
+                ValueKind::Table(map) => Ok(map),
+                _ => unreachable!(),
+            },
+            JsonValue::Array(_) => Err(Unexpected::Seq),
+            JsonValue::Null => Err(Unexpected::Unit),
+        },
+        None => Err(Unexpected::Unit),
+    }
+    .map_err(|err| ConfigError::invalid_root(uri, err))
+    .map_err(|err| Box::new(err) as Box<dyn Error + Send + Sync>)
+}
+
+fn from_jsonc_value(uri: Option<&String>, value: JsonValue) -> Value {
+    let vk = match value {
+        JsonValue::Null => ValueKind::Nil,
+        JsonValue::String(v) => ValueKind::String(v.to_string()),
+        JsonValue::Number(ref value) => {
+            if let Ok(value) = value.parse::<i64>() {
+                ValueKind::I64(value)
+            } else if let Ok(value) = value.parse::<f64>() {
+                ValueKind::Float(value)
+            } else {
+                unreachable!();
+            }
+        },
+        JsonValue::Boolean(v) => ValueKind::Boolean(v),
+        JsonValue::Object(table) => {
+            let m = table
+                .into_iter()
+                .map(|(k, v)| (k, from_jsonc_value(uri, v)))
+                .collect();
+            ValueKind::Table(m)
+        }
+        JsonValue::Array(array) => {
+            let l = array
+                .into_iter()
+                .map(|v| from_jsonc_value(uri, v))
+                .collect();
+            ValueKind::Array(l)
+        }
+    };
+    Value::new(uri, vk)
+}
