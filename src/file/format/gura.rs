04254a86eb573eaad984f786009e320ed5e26190
polarathene <5098581+polarathene@users.noreply.github.com>
2023-10-07 18:43:27 +1300

    refactor: `from_gura_value` (json referenced)
    This method seems to be adapted from `format/json.rs:from_json_value()`.
    - I adjusted the order of matched types to mirror that of `format/json.rs`.
    - `val` => `value`.
    - No need to dereference values, in this case we consume the `value` parameter,
      rather than expect a reference to borrow.
      No need to use `ref` or `clone()` to create owned values.
    - Potential Improvement: Adopt the iter + collect pattern used by other formats
      instead of the `for` loop used for array and object types here.
    
    Signed-off-by: Brennan Kinney <5098581+polarathene@users.noreply.github.com>

--- src/file/format/gura.rs
+++ src/file/format/gura.rs
@@ -9,49 +9,46 @@ pub fn parse(
     uri: Option<&String>,
     text: &str,
 ) -> Result<Map<String, Value>, Box<dyn Error + Send + Sync>> {
-    let val = from_gura_value(uri, &gura::parse(text).unwrap());
-
-    match val.kind {
+    let value = from_gura_value(uri, gura::parse(text).unwrap());
+    match value.kind {
         ValueKind::Table(map) => Ok(map),
 
         _ => Ok(Map::new()),
     }
 }
 
-fn from_gura_value(uri: Option<&String>, val: &GuraType) -> Value {
-    match val {
-        GuraType::Null => Value::new(uri, ValueKind::Nil),
+fn from_gura_value(uri: Option<&String>, value: GuraType) -> Value {
+    match value {
+        GuraType::String(value) => Value::new(uri, ValueKind::String(value)),
 
-        GuraType::Object(ref table) => {
+        GuraType::Integer(value) => Value::new(uri, ValueKind::I64(value as i64)),
+        GuraType::BigInteger(value) => Value::new(uri, ValueKind::I128(value)),
+        GuraType::Float(value) => Value::new(uri, ValueKind::Float(value)),
+
+        GuraType::Bool(value) => Value::new(uri, ValueKind::Boolean(value)),
+
+        GuraType::Object(table) => {
             let mut m = Map::new();
 
-            for (key, val) in table {
-                m.insert(key.clone(), from_gura_value(uri, val));
+            for (key, value) in table {
+                m.insert(key, from_gura_value(uri, value));
             }
 
             Value::new(uri, ValueKind::Table(m))
         }
 
-        GuraType::Bool(val) => Value::new(uri, ValueKind::Boolean(*val)),
-
-        GuraType::String(ref val) => Value::new(uri, ValueKind::String(val.clone())),
-
-        GuraType::Integer(val) => Value::new(uri, ValueKind::I64(*val as i64)),
-
-        GuraType::BigInteger(val) => Value::new(uri, ValueKind::I128(*val)),
-
-        GuraType::Float(val) => Value::new(uri, ValueKind::Float(*val)),
-
-        GuraType::Array(ref arr) => {
+        GuraType::Array(array) => {
             let mut l = Vec::new();
 
-            for val in arr {
-                l.push(from_gura_value(uri, val));
+            for value in array {
+                l.push(from_gura_value(uri, value));
             }
 
             Value::new(uri, ValueKind::Array(l))
         }
 
+        // Null or remaining types (only intended for internal use):
+        GuraType::Null => Value::new(uri, ValueKind::Nil),
         _ => Value::new(uri, ValueKind::Nil),
     }
 }
