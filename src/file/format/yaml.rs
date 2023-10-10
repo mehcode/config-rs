cb27a47cc0609c2a08b0d74deb417fa525117c3b
polarathene <5098581+polarathene@users.noreply.github.com>
2023-10-10 17:04:17 +1300

    fix: Change YAML crate to `serde_yaml`
    - `yaml-rust` is unmaintained for over 2 years.
    - Like with `yaml-rust`, requires special handling for table keys.
    
    Signed-off-by: Brennan Kinney <5098581+polarathene@users.noreply.github.com>

--- src/file/format/yaml.rs
+++ src/file/format/yaml.rs
@@ -1,8 +1,4 @@
 use std::error::Error;
-use std::fmt;
-use std::mem;
-
-use yaml_rust as yaml;
 
 use crate::format;
 use crate::map::Map;
@@ -12,94 +8,53 @@ pub fn parse(
     uri: Option<&String>,
     text: &str,
 ) -> Result<Map<String, Value>, Box<dyn Error + Send + Sync>> {
-    // Parse a YAML object from file
-    let mut docs = yaml::YamlLoader::load_from_str(text)?;
-    let root = match docs.len() {
-        0 => yaml::Yaml::Hash(yaml::yaml::Hash::new()),
-        1 => mem::replace(&mut docs[0], yaml::Yaml::Null),
-        n => {
-            return Err(Box::new(MultipleDocumentsError(n)));
-        }
-    };
-
-    let value = from_yaml_value(uri, &root)?;
+    // Parse a YAML input from the provided text
+    let value = from_yaml_value(uri, serde_yaml::from_str(text)?);
     format::extract_root_table(uri, value)
 }
 
-fn from_yaml_value(
-    uri: Option<&String>,
-    value: &yaml::Yaml,
-) -> Result<Value, Box<dyn Error + Send + Sync>> {
-    match *value {
-        yaml::Yaml::String(ref value) => Ok(Value::new(uri, ValueKind::String(value.clone()))),
-        yaml::Yaml::Real(ref value) => {
-            // TODO: Figure out in what cases this can panic?
-            value
-                .parse::<f64>()
-                .map_err(|_| {
-                    Box::new(FloatParsingError(value.to_string())) as Box<(dyn Error + Send + Sync)>
+pub fn from_yaml_value(uri: Option<&String>, value: serde_yaml::Value) -> Value {
+    let vk = match value {
+        serde_yaml::Value::Tagged(_) | serde_yaml::Value::Null => ValueKind::Nil,
+        serde_yaml::Value::Bool(v) => ValueKind::Boolean(v),
+        serde_yaml::Value::String(v) => ValueKind::String(v),
+
+        serde_yaml::Value::Number(v) => {
+            if v.is_i64() {
+                ValueKind::I64(v.as_i64().expect("i64"))
+            } else if v.is_u64() {
+                ValueKind::U64(v.as_u64().expect("u64"))
+            } else if v.is_f64() {
+                ValueKind::Float(v.as_f64().expect("f64"))
+            } else {
+                ValueKind::Nil
+            }
+        }
+
+        serde_yaml::Value::Mapping(table) => {
+            let m = table
+                .into_iter()
+                .map(|(k, v)| {
+                    let key = match k {
+                        serde_yaml::Value::Number(v) => v.to_string(),
+                        serde_yaml::Value::String(v) => v,
+
+                        _ => unreachable!(),
+                    };
+                    let value = from_yaml_value(uri, v);
+                    (key, value)
                 })
-                .map(ValueKind::Float)
-                .map(|f| Value::new(uri, f))
-        }
-        yaml::Yaml::Integer(value) => Ok(Value::new(uri, ValueKind::I64(value))),
-        yaml::Yaml::Boolean(value) => Ok(Value::new(uri, ValueKind::Boolean(value))),
-        yaml::Yaml::Hash(ref table) => {
-            let mut m = Map::new();
-            for (key, value) in table {
-                match key {
-                    yaml::Yaml::String(k) => m.insert(k.to_owned(), from_yaml_value(uri, value)?),
-                    yaml::Yaml::Integer(k) => m.insert(k.to_string(), from_yaml_value(uri, value)?),
-                    _ => unreachable!(),
-                };
-            }
-            Ok(Value::new(uri, ValueKind::Table(m)))
-        }
-        yaml::Yaml::Array(ref array) => {
-            let mut l = Vec::new();
+                .collect();
 
-            for value in array {
-                l.push(from_yaml_value(uri, value)?);
-            }
-
-            Ok(Value::new(uri, ValueKind::Array(l)))
+            ValueKind::Table(m)
         }
 
-        // 1. Yaml NULL
-        // 2. BadValue – It shouldn't be possible to hit BadValue as this only happens when
-        //               using the index trait badly or on a type error but we send back nil.
-        // 3. Alias – No idea what to do with this and there is a note in the lib that its
-        //            not fully supported yet anyway
-        _ => Ok(Value::new(uri, ValueKind::Nil)),
-    }
-}
+        serde_yaml::Value::Sequence(array) => {
+            let l = array.into_iter().map(|v| from_yaml_value(uri, v)).collect();
 
-#[derive(Debug, Copy, Clone)]
-struct MultipleDocumentsError(usize);
+            ValueKind::Array(l)
+        }
+    };
 
-impl fmt::Display for MultipleDocumentsError {
-    fn fmt(&self, format: &mut fmt::Formatter) -> fmt::Result {
-        write!(format, "Got {} YAML documents, expected 1", self.0)
-    }
-}
-
-impl Error for MultipleDocumentsError {
-    fn description(&self) -> &str {
-        "More than one YAML document provided"
-    }
-}
-
-#[derive(Debug, Clone)]
-struct FloatParsingError(String);
-
-impl fmt::Display for FloatParsingError {
-    fn fmt(&self, format: &mut fmt::Formatter) -> fmt::Result {
-        write!(format, "Parsing {} as floating point number failed", self.0)
-    }
-}
-
-impl Error for FloatParsingError {
-    fn description(&self) -> &str {
-        "Floating point number parsing failed"
-    }
+    Value::new(uri, vk)
 }
