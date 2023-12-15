e30debf7cab0b4b8e0edbd254cdc58edc24ab7b2
mfirhas <mfathirirhas@gmail.com>
2023-12-15 08:18:03 +0700

    Add try parse ini
    Parse non-string from env/ini file into the types

--- src/file/format/ini.rs
+++ src/file/format/ini.rs
@@ -16,22 +16,29 @@ pub fn parse(
             Some(sec) => {
                 let mut sec_map: Map<String, Value> = Map::new();
                 for (k, v) in prop.iter() {
-                    sec_map.insert(
-                        k.to_owned(),
-                        Value::new(uri, ValueKind::String(v.to_owned())),
-                    );
+                    sec_map.insert(k.to_owned(), Value::new(uri, try_parse(v)));
                 }
                 map.insert(sec.to_owned(), Value::new(uri, ValueKind::Table(sec_map)));
             }
             None => {
                 for (k, v) in prop.iter() {
-                    map.insert(
-                        k.to_owned(),
-                        Value::new(uri, ValueKind::String(v.to_owned())),
-                    );
+                    map.insert(k.to_owned(), Value::new(uri, try_parse(v)));
                 }
             }
         }
     }
     Ok(map)
+}
+
+fn try_parse(s: &str) -> ValueKind {
+    if let Ok(parsed) = s.parse::<bool>() {
+        return ValueKind::Boolean(parsed);
+    }
+    if let Ok(parsed) = s.parse::<i64>() {
+        return ValueKind::I64(parsed);
+    }
+    if let Ok(parsed) = s.parse::<f64>() {
+        return ValueKind::Float(parsed);
+    }
+    ValueKind::String(s.to_string())
 }
---
+++ tests/file_ini_2.rs
@@ -0,0 +1,72 @@
+use config::{
+    builder::DefaultState, Config, ConfigBuilder, ConfigError, Environment, File, FileFormat,
+};
+use serde::Deserialize;
+use std::fmt::{Debug, Display};
+
+const ALL_CFG_FILE: &str = "./tests/all.env";
+
+#[derive(Debug, Deserialize)]
+struct TestConfig {
+    #[serde(alias = "TEST__NUMBER")]
+    pub number: i32,
+    #[serde(alias = "TEST__STRING")]
+    pub string: String,
+    #[serde(alias = "TEST__FLOAT")]
+    pub float: f32,
+    #[serde(alias = "TEST__BOOLEAN")]
+    pub boolean: bool,
+    #[serde(flatten)]
+    pub segment: TestSegmentConfig,
+}
+
+#[derive(Debug, Deserialize)]
+struct TestSegmentConfig {
+    #[serde(alias = "TEST__SEGMENT_NUMBER")]
+    pub segment_number: i32,
+    #[serde(alias = "TEST__SEGMENT_STRING")]
+    pub segment_string: String,
+    #[serde(alias = "TEST__SEGMENT_FLOAT")]
+    pub segment_float: f32,
+    #[serde(alias = "TEST__SEGMENT_BOOLEAN")]
+    pub segment_boolean: bool,
+    #[serde(flatten)]
+    pub segment_child: TestSegmentChildConfig,
+    #[serde(flatten)]
+    pub segment_child2: TestSegmentChildConfig2,
+}
+
+#[derive(Debug, Deserialize)]
+struct TestSegmentChildConfig {
+    #[serde(alias = "TEST__SEGMENT_CHILD_BOOLEAN")]
+    pub segment_child_bool: bool,
+    #[serde(alias = "TEST__SEGMENT_CHILD_FLOAT")]
+    pub segment_child_float: f64,
+}
+
+#[derive(Debug, Deserialize)]
+struct TestSegmentChildConfig2 {
+    #[serde(alias = "TEST__SEGMENT_CHILD2_BOOLEAN")]
+    pub segment_child_number: u64,
+    #[serde(alias = "TEST__SEGMENT_CHILD2_FLOAT")]
+    pub segment_child_float: f64,
+    #[serde(flatten)]
+    pub segment_child2_child: TestSegmentChildConfig2Child,
+}
+
+#[derive(Debug, Deserialize)]
+struct TestSegmentChildConfig2Child {
+    #[serde(alias = "TEST__SEGMENT_CHILD2_CHILD_FLOAT")]
+    pub segment_child2_child: f32,
+}
+
+#[test]
+fn test_file_config_success() {
+    let cfg = Config::builder()
+        .add_source(config::File::new(ALL_CFG_FILE, FileFormat::Ini))
+        .build()
+        .unwrap()
+        .try_deserialize::<TestConfig>();
+    dbg!(&cfg);
+    assert!(cfg.is_ok());
+}
